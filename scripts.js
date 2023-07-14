ea1318c9bf5edd4b6a1532a3d59f4f1b33da6239
Nikolai <nikolaiptukhin@gmail.com>
2023-07-13 22:37:45 -0400

    new vers

--- scripts.js
+++ scripts.js
@@ -1,11 +1,60 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-function init () {
-    let takeOffButton = document.getElementById("takeoff");
+window.addEventListener('load',function(){
+    const takeoff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
+    const up = document.getElementById("up");
+    const down = document.getElementById("down");
+    const right = document.getElementById("right");
+    const left = document.getElementById("left");
+
+
     
+    takeoff.addEventListener("click", function(){
+        window.confirm("Confirm the shuttle is ready for takeoff")
+        if (takeoff){
+            flightStatus.innerHTML = "Shuttle in Flight";
+            shuttleBackground.style.backgroundColor = 'blue';
+            spaceShuttleHeight.innerHTML = "10,000"
+        }
+        
+    });
 
-    takeOffButton.addEventListener("click", function(event){
-        console.log ("Confurm that the shuttle is ready for takeoff.");
+    landing.addEventListener("click", function(){
+        window.alert("The shuttle is landing. Landing gear engaged.")
+        
+            flightStatus.innerHTML =  "The shuttle has landed.";
+            shuttleBackground.style.backgroundColor = 'green';
+            spaceShuttleHeight.innerHTML = "0"
+        });
 
-    })
-} window.addEventListener ("load", init);
+        missionAbort.addEventListener("click", function(){
+            window.confirm("Confirm that you want to abort the mission.")
+            if (missionAbort){
+                flightStatus.innerHTML = "Mission aborted.";
+                shuttleBackground.style.backgroundColor = 'green';
+                spaceShuttleHeight.innerHTML = "0"
+            }
+            
+        });
+
+        up.addEventListener("click", function(){
+            
+            
+            
+            
+        });
+
+        up.addEventListener("click", function(){
+            
+            
+            
+            
+        });
+
+
+});
