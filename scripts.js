92948efbfda3b1f6cdc775ade81816c8ab02ec09
Nikolai <nikolaiptukhin@gmail.com>
2023-07-13 22:24:36 -0400

    test exercise

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+    let takeOffButton = document.getElementById("takeoff");
+    
+
+    takeOffButton.addEventListener("click", function(event){
+        console.log ("Confurm that the shuttle is ready for takeoff.");
+
+    })
+} window.addEventListener ("load", init);
